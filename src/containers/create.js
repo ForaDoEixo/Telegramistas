a01ebf32d8c279312b52d55ea61431475906c98d
Niv Sardi <xaiki@evilgiggle.com>
2018-06-29 19:43:12 -0300

    random names
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/create.js
+++ src/containers/create.js
@@ -3,11 +3,20 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import random from 'random'
+
 import identities from '../reducers/identities'
 import Create from '../components/create';
+import feministas from '../feministas.json'
+
+const pickRandom = (array) => (
+    array[random.int(0, array.length - 1)]
+)
+
+const makeName = () => (`${pickRandom(feministas.name)} ${pickRandom(feministas.surname)}`)
 
 const ConnectedCreate = connect(
-    undefined,
+    () => ({name: makeName()}),
     (dispatch) => ({actions: bindActionCreators(identities.actions, dispatch)})
 )(Create)
 
